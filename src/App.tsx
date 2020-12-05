af6176878157a67089dfb4a0966caa8393ac65d9
@theship <juburdekin@gmail.com, julee@abevy.com>
2020-12-05 11:12:25 -0800

    Added ColorPicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import IntarsiaGrid from "./components/IntarsiaGrid"
+import ColorPicker from "./components/ColorPicker"
 
 export default function App() {
     return (
       <div className="App">
         <IntarsiaGrid />
+        <ColorPicker />
       </div>
     );
 }
---
+++ src/components/ColorPicker.tsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+import { SketchPicker } from "react-color";
+
+interface IStateColor {
+    setColor: string,
+}
+interface IStateShowPicker {
+    setShowColorPicker: boolean,
+}
+
+const ColorPicker: React.FC = () => {
+    const [color, setColor] = useState<IStateColor | string>("#DB1374")
+    const [showColorPicker, setShowColorPicker] = useState<IStateShowPicker | boolean>(false)
+
+    useEffect(() => {
+        setColor(color)
+    }, [color])
+
+    return (
+        <div className="ColorPicker">
+            <button onClick={() => setShowColorPicker(showColorPicker => !showColorPicker)}>
+                {showColorPicker ? 'Close' : 'Color'}
+            </button>
+
+            {showColorPicker && (
+                <SketchPicker
+                    onChange={updatedColor => setColor(updatedColor)}
+                    color={color}
+                />
+            )}
+        </div>
+    );
+}
+export default ColorPicker;
